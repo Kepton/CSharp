229a29d63259984c7d953197578ee70eca405f00
kepton <liuyalongscu@foxmail.com>
2014-11-23 23:27:10 +0800

    对Delegate  Lamdba   Event学习

---
+++ CSharp/DelegateLamdbaEvent/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLamdbaEvent
+{
+    class Date
+    {
+        string year;
+        string month;
+        public Date(string _y, string _m)
+        {
+            this.year = _y;
+            this.month = _m;
+        }
+
+        public override string ToString()
+        {
+            return year+"年"+month+"月";
+        }
+    }
+    class Program
+    {
+        delegate double ReturnDouble();
+        delegate string GetAString();
+        static void Main(string[] args)
+        {
+            int x = 0;
+            GetAString str = new GetAString(x.ToString);
+            Console.WriteLine(str());
+
+            Date date = new Date("2014", "11");
+
+            str = date.ToString;
+
+            Console.WriteLine(str());
+
+
+            Console.ReadKey();
+        }
+    }
+}
