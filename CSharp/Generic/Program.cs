6fbfd9269039a6a3e16fb4b92aceda63bc73fd77
Kepton <liuyalongscu@foxmail.com>
2015-09-23 15:08:29 +0800

    Generic

--- CSharp/Generic/Program.cs
+++ CSharp/Generic/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Generic
 {
+    class myclass
+    {
+ 
+    }
+
+
+    public class myList<c>
+    {
+        public c value;
+
+        public c getValue()
+        {
+            return value;
+        }
+    }
+
     class Program
     {
+        
         static void Main(string[] args)
         {
+            var kk = new myList<int>();
+            kk.value = 30;
+
+            Console.WriteLine(kk.getValue());
+
+            Console.ReadKey();
         }
     }
 }
